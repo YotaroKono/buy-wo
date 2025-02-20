500b39c0c1d23b1e3aedd006501e3084ec87dc7d
YotaroKono <yotarokono@gmail.com>
2025-02-20 23:06:21 +0900

    fix: auth server

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,21 +1,54 @@
+import { createCookieSessionStorage } from "@remix-run/node";
 import { Authenticator } from "remix-auth";
 import { Auth0Strategy } from "remix-auth-auth0";
 
 // Authenticatorのインスタンスを作成し、ストラテジーが返す型と
 // セッションに保存される型のジェネリックを渡します
-export const authenticator = new Authenticator<User>();
+
+type User = {
+  email: string;
+};
+
+export let sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "_session", // use any name you want here
+    sameSite: "lax", // this helps with CSRF
+    path: "/", // remember to add this so the cookie will work in all routes
+    httpOnly: true, // for security reasons, make this cookie http only
+    secrets: ["SeCreT"], // replace this with an actual secret (よくわからん。多分なんでもいい)
+    secure: process.env.NODE_ENV === "production", // enable this in prod only
+  },
+});
+
+export const { getSession, commitSession, destroySession } = sessionStorage;
+
+const UserService = {
+  async findOrCreate(data: { email: string }): Promise<User> {
+    // ここでユーザーを検索または作成するロジックを実装
+    // 例: データベースとの連携など
+
+    return {
+      email: data.email,
+    };
+  },
+};
+
+export const authenticator = new Authenticator<User>(sessionStorage);
 
 const auth0Strategy = new Auth0Strategy(
-	{
-		callbackURL: "https://example.com/auth/auth0/callback",
-		clientID: "YOUR_AUTH0_CLIENT_ID",
-		clientSecret: "YOUR_AUTH0_CLIENT_SECRET",
-		domain: "YOUR_TENANT.us.auth0.com",
-	},
-	async ({ accessToken, refreshToken, extraParams, profile }) => {
-		// トークンとプロファイルを使用して、DBまたはAPIからユーザーデータを取得
-		return User.findOrCreate({ email: profile.emails[0].value });
-	},
+  {
+    callbackURL: "https://example.com/auth/auth0/callback",
+    clientID: "AUTH0_CLIENT_ID",
+    clientSecret: "AUTH0_CLIENT_SECRET",
+    domain: "DOMAIN",
+  },
+  async ({ accessToken, refreshToken, extraParams, profile }) => {
+    console.log("profile", profile);
+    if (!profile.emails || profile.emails.length === 0) {
+      throw new Error("Email is required");
+    }
+    return UserService.findOrCreate({ email: profile.emails[0].value });
+  }
 );
 
 authenticator.use(auth0Strategy);
