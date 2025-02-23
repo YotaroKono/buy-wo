c890f6f5110bf6bf104bcdf228dd8572f0e66537
YotaroKono <yotarokono@gmail.com>
2025-02-24 00:53:49 +0900

    fix comment

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -15,7 +15,7 @@ export let sessionStorage = createCookieSessionStorage({
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
-    secrets: ["SeCreT"], // replace this with an actual secret (よくわからん。多分なんでもいい)
+    secrets: ["SeCreT"], // replace this with an actual secret
     secure: process.env.NODE_ENV === "production", // enable this in prod only
   },
 });
