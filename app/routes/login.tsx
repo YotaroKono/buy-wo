49da0f1b264040e8863235937359a64ef3e999ae
YotaroKono <yotarokono@gmail.com>
2025-02-24 00:52:54 +0900

    fix login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,18 @@
 import { Form } from "@remix-run/react";
 
+import { type ActionFunctionArgs, redirect } from "@remix-run/node";
+
+import { authenticator } from "~/utils/auth.server";
+
+// export const loader = () => redirect("/hoge");
+
+export const action = ({ request }: ActionFunctionArgs) => {
+  return authenticator.authenticate("auth0", request);
+};
+
 export default function Login() {
   return (
-    <Form action="/auth/auth0" method="post">
+    <Form action="/login" method="post">
       <button>Login with Auth0</button>
     </Form>
   );
