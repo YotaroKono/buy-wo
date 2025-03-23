7b12b11f52f0f1361d641b130c9a36ba77fa4033
YotaroKono <yotarokono@gmail.com>
2025-03-23 11:10:54 +0900

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
@@ -18,7 +19,23 @@ export default defineConfig({
 				v3_singleFetch: true,
 				v3_lazyRouteDiscovery: true,
 			},
+			// .server.tsファイルをクライアントバンドルから除外
+			serverModuleFormat: "esm",
 		}),
 		tsconfigPaths(),
-	],
+		nodePolyfills({
+			include: ['crypto', 'buffer', 'process', 'stream', 'util'],
+			globals: { 
+			  Buffer: true,
+			  global: true,
+			  process: true 
+			},
+		  }),
+		],
+	//.server.tsファイルを明示的に外部化
+	build: {
+			rollupOptions: {
+			  external: [/\.server\.(js|jsx|ts|tsx)$/],
+			},
+		  },
 });
