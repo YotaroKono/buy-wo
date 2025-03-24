49ac7988d8f48259eeda263402fa3fc25b9b15a8
YotaroKono <yotarokono@gmail.com>
2025-03-24 17:32:12 +0900

    add try catch

--- app/utils/supabase.server.ts
+++ app/utils/supabase.server.ts
@@ -2,15 +2,19 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabaseClient(supabaseToken: string) {
-  return createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY,
-    {
-      global: {
-        headers: {
-          Authorization: `Bearer ${supabaseToken}`,
-        },
-      },
+    try {
+        return createClient(
+            process.env.SUPABASE_URL,
+            process.env.SUPABASE_ANON_KEY,
+            {
+                global: {
+                    headers: {
+                        Authorization: `Bearer ${supabaseToken}`,
+                    },
+                },
+            }
+        );
+    } catch (error) {
+        console.error("Error creating Supabase client:", error);
     }
-  );
 }
