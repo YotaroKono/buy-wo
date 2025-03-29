0cbdc789f26fc49d1b14ac1835e9b9031a7425e0
YotaroKono <yotarokono@gmail.com>
2025-03-29 23:26:57 +0900

    fix logintsx

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,21 +1,8 @@
-import { Form } from "@remix-run/react";
-
 import { type ActionFunctionArgs, redirect } from "@remix-run/node";
 
 import { authenticator } from "~/utils/auth.server";
-
-// export const loader = () => redirect("/hoge");
 
 export const action = ({ request }: ActionFunctionArgs) => {
 	console.log("request", request);
 	return authenticator.authenticate("auth0", request);
 };
-
-// TODO: ログイン機能はヘッダーで実装する・ログイン後のリダイレクトはホーム画面にしてモーダルを表示する
-export default function Login() {
-	return (
-		<Form action="/login" method="post">
-			<button type="submit">Login with Auth0</button>
-		</Form>
-	);
-}
