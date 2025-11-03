bd5d6b2cd254f4742335c5a3dcea1116159b665e
YotaroKono <yotarokono@gmail.com>
2025-11-03 21:36:21 +0900

    refactor: replace process.env with env module in logout action

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,13 +1,14 @@
 import { type ActionFunctionArgs, redirect } from "@remix-run/node";
 
 import { destroySession, getSession } from "~/models/auth.server";
+import { env } from "~/models/env.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
 	const session = await getSession(request.headers.get("Cookie"));
-	const logoutURL = new URL(process.env.AUTH0_LOGOUT_URL); // i.e https://YOUR_TENANT.us.auth0.com/v2/logout
+	const logoutURL = new URL(env.AUTH0_LOGOUT_URL); // i.e https://YOUR_TENANT.us.auth0.com/v2/logout
 
-	logoutURL.searchParams.set("client_id", process.env.AUTH0_CLIENT_ID);
-	logoutURL.searchParams.set("returnTo", process.env.AUTH0_RETURN_TO_URL);
+	logoutURL.searchParams.set("client_id", env.AUTH0_CLIENT_ID);
+	logoutURL.searchParams.set("returnTo", env.AUTH0_RETURN_TO_URL);
 
 	return redirect(logoutURL.toString(), {
 		headers: {
